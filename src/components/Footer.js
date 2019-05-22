df11240779c890b4ddce5681ffae54a385a87489
Nick Wild <nick@nickwild.com>
2019-05-22 09:18:00 +0100

    added twitter widget.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import TwitterFeed from './TwitterFeed';
 
 const Footer = () => (
-  <div className="footer">
-    <div className="has-text-white-ter">Footer In Here</div>
+  <div className="footer has-text-white-ter">
+    <div className="container is-fluid">
+
+      <div className="columns">
+        <div className="column is-one-third-tablet is-one-third-desktop is-narrow">
+      Stuff in here
+        </div>
+        <div className="column is-one-third-tablet is-one-third-desktop is-narrow">
+      Stuff in here
+        </div>
+        <div className="column is-one-third-tablet is-one-third-desktop is-narrow">
+          <h3 className="is-size-5 has-text-weight-bold">Recent Tweets</h3>
+          <TwitterFeed />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
