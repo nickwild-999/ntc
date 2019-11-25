bb8410e97a00ee117719c311ece25a4cc52fef8f
Nick Wild <nick@nickwild.com>
2019-11-25 10:02:35 +0000

    v1.17 changed directors to css grid

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -3,18 +3,20 @@ import styled from 'styled-components';
 
 
 export default function Directors() {
-  const directorlist = ['Gus Van Sant', 'Keith McCarthy', 'Jacques Salmon', 'Peter Lydon', 'Lisa Gunning', 'Shane Meadows', 'Michael Cumming', 'Rob Chiu', 'Nick Wild', 'Thomas James', 'Odilon Rocha', 'Kim Gehring', 'Betsan Morris Evans', 'Kavi Raz', 'Paul Tanter', 'Ryan Bonder', 'Daniel Wolfe', 'Tom King', 'Peter Cattaneo', 'Tony Kaye', 'Paul Hunter', 'Chris Morris', 'Yousaf Ali Khan', 'Matt Winn', 'Julian Kronfli', 'Phil Taylor', 'Tony Durston', 'Emma Farrell', 'Mick Davies', 'Martin Brieley', 'Dahlan Masselle', 'Ian Single', 'Phil Kerr', 'Bluey', 'Steve Wright', 'Richard Laxton', 'Pat Holden', 'Matt Carter', 'Steve Bendelack', 'Mark Brozel', 'Michael Baldwin', 'Michael Winterbottom', 'WHO', 'Darren Bolton'];
+  const directorlist = ['Gus Van Sant', 'Keith McCarthy', 'Jacques Salmon', 'Peter Lydon', 'Lisa Gunning', 'Shane Meadows', 'Michael Cumming', 'Rob Chiu', 'Nick Wild', 'Thomas James', 'Odilon Rocha', 'Kim Gehring', 'Betsan Morris Evans', 'Kavi Raz', 'Paul Tanter', 'Ryan Bonder', 'Daniel Wolfe', 'Tom King', 'Peter Cattaneo', 'Tony Kaye', 'Paul Hunter', 'Chris Morris', 'Yousaf Ali Khan', 'Matt Winn', 'Julian Kronfli', 'Phil Taylor', 'Tony Durston', 'Emma Farrell', 'Mick Davies', 'Martin Brieley', 'Dahlan Masselle', 'Ian Single', 'Phil Kerr', 'Bluey', 'Steve Wright', 'Richard Laxton', 'Pat Holden', 'Matt Carter', 'Steve Bendelack', 'Mark Brozel', 'Michael Baldwin', 'WHO', 'Michael Winterbottom', 'Darren Bolton'];
   return (
     <>
-      <div className="directors-wrapper">
+      <div className="directors-wrapper2">
         {directorlist.map(director => (
           <>
             <span className="directors-name">
+            &bull;
+              {' '}
               {director}
             </span>
-            <span>
+            {/* <span>
             &bull;
-            </span>
+            </span> */}
           </>
         )) }
       </div>
